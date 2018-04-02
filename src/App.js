1236cd0885d81b7b31db3fb0ad632a889e924fed
kom <komphet@kmi.tl>
2018-04-02 21:02:27 +0700

    Create Repository

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor(props){
+        super(props);
+
+        this.state = {
+            name: "komphet"
+        }
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+                <input type="text" onChange={(e)=>{ this.setState({name: e.target.value}) }}/>
+                <div>{this.state.name}</div>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/components/InputForm.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class InputForm extends Component {
+
+    render(){
+        return (
+            <div>
+                <input type="text"/>
+            </div>
+        )
+    }
+}
+
+export default InputForm;
